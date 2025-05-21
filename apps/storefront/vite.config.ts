224eaa016d62dcbaff1f27f7dcc02f3d5823755d
Max Maher <mmaher@four51.com>
2025-05-21 11:21:28 -0500

    Testing Vercel routing fix

--- apps/storefront/vite.config.ts
+++ apps/storefront/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   server: {
     port: 3001,
   },
   plugins: [splitVendorChunkPlugin(), react()],
-})
+});
